c7a90fdfd8c1745aa45e1ae3c077e1f61cec1f78
Chloe-adnet-eng <chloe.adnet@ext.bpifrance.fr>
2025-03-25 21:50:13 +0100

    feat(cplusplus): optimize code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include "case.hpp"
+#include <iostream>
 
-#define WINDOW_COTE 1000
-#define CASE_LIGNE 10
-#define CASE_COLONNE 10
+#define WINDOW_COTE 400
+#define NOMBRE_CASE_COTE 400
 
 using namespace std;
 using std::copy;
@@ -13,17 +13,19 @@ int main()
 {
     srand(time(0));
     sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({WINDOW_COTE, WINDOW_COTE}), "Grille Jeu de la vie");
-    vector<vector<Case>> tableau;
+    // Pré-allocation de la mémoire
+    vector<Case> initial_board(NOMBRE_CASE_COTE * NOMBRE_CASE_COTE);
+    int rectangle_length = WINDOW_COTE/NOMBRE_CASE_COTE;
 
-    for (int i=0; i<WINDOW_COTE/CASE_LIGNE; i++){
-        vector<Case> ligne = vector<Case> {};
-        tableau.push_back(ligne);
-        for (int j=0; j<WINDOW_COTE/CASE_COLONNE; j++){
-            Case current_case = Case((float) i*CASE_LIGNE, (float) j*CASE_COLONNE);
-            tableau[i].push_back(current_case);
+    for (int i = 0; i < NOMBRE_CASE_COTE; i++) {
+        for (int j = 0; j < NOMBRE_CASE_COTE; j++) {
+            initial_board[i * NOMBRE_CASE_COTE + j] = Case(i * rectangle_length, j * rectangle_length);
         }
     }
 
+    sf::Clock clock;
+    float frame_rate = 0.1f;
+   
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -35,17 +37,23 @@ int main()
             }
         }
 
-        vector<vector<Case>> next_tableau = tableau;
-        
-        for (int i=0; i<WINDOW_COTE/CASE_LIGNE; i++){
-            for (int j=0; j<WINDOW_COTE/CASE_COLONNE; j++){
-                tableau[i][j].PrintCase(window);
-                next_tableau[i][j].UpdateState(tableau);
+        if (clock.getElapsedTime().asSeconds() >= frame_rate) {
+            vector<Case> next_board = initial_board;
+
+            for (int i = 0; i < NOMBRE_CASE_COTE; i++) {
+                for (int j = 0; j < NOMBRE_CASE_COTE; j++) {
+                    int idx = i * NOMBRE_CASE_COTE + j;
+                    initial_board[idx].PrintCase(window);  // Dessiner la case
+                    next_board[idx].UpdateState(initial_board);  // Mettre à jour l'état de la case
+                }
             }
+
+            initial_board = next_board;  // Mise à jour de l'état pour la prochaine itération
+
+            clock.restart();  // Réinitialiser le chrono
+
+            window.display();  // Affichage
         }
 
-        tableau = next_tableau;
-        
-        window.display();
     }
 }
